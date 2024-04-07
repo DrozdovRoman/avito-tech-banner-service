4a986bf62fd5ac79cb9bb80502c5c422028fe9b4
Roman Drozdov <romandrozdov@icloud.com>
2024-04-07 19:12:09 +0500

    The implementation of the DI container Uber/fx in the main function.

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -5,43 +5,48 @@ import (
 	"github.com/DrozdovRoman/avito-tech-banner-service/internal/application/common/configuration"
 	"github.com/DrozdovRoman/avito-tech-banner-service/internal/presentation/http"
 	"github.com/sirupsen/logrus"
-	"os"
-	"os/signal"
-	"syscall"
-	"time"
+	fxlogrus "github.com/takt-corp/fx-logrus"
+	"go.uber.org/fx"
+	"go.uber.org/fx/fxevent"
 )
 
 var configFile = ""
 
 // @title           Banner Service
 // @version         1.0
-// @description     How many Scots does it take to change a light bulb?
+// @description     How many Avito developers does it take to change a light bulb? (Interview answer <3)
 
 // @contact.name   Drozdov Roman
 // @contact.email  romandrozdov@icloud.com
 func main() {
-	logrus.SetLevel(logrus.InfoLevel)
-	logrus.SetFormatter(&logrus.TextFormatter{})
-
 	config, err := configuration.LoadConfiguration(configFile)
 	if err != nil {
-		logrus.WithError(err).Fatal("Unable to load configuration")
-	}
-	logrus.Info("Configuration loaded successfully")
-
-	server := http.NewHttpServer(config)
-	server.Start()
-
-	stopChan := make(chan os.Signal, 1)
-	signal.Notify(stopChan, os.Interrupt, syscall.SIGTERM)
-	<-stopChan
-
-	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
-	defer cancel()
-	if err := server.Stop(ctx); err != nil {
-		logrus.WithError(err).Fatal("Failed to gracefully stop the server")
-	} else {
-		logrus.Info("Server stopped gracefully")
+		logrus.WithError(err).Fatal("unable to load configuration")
 	}
 
+	fx.New(
+		fx.WithLogger(func() fxevent.Logger {
+			return &fxlogrus.LogrusLogger{Logger: logrus.StandardLogger()}
+		}),
+		fx.Supply(config),
+		fx.Provide(
+			// Presentation | API
+			http.NewHttpServer,
+			http.NewRouter,
+		),
+		fx.Invoke(
+			// http
+			func(lc fx.Lifecycle, httpServer *http.Server) {
+				logrus.Println("Starting HTTP server")
+				lc.Append(fx.Hook{
+					OnStart: func(ctx context.Context) error {
+						return httpServer.Start(ctx)
+					},
+					OnStop: func(ctx context.Context) error {
+						return httpServer.Stop(ctx)
+					},
+				})
+			},
+		),
+	).Run()
 }
--- go.mod
+++ go.mod
@@ -23,6 +23,9 @@ require (
 	github.com/jackc/pgservicefile v0.0.0-20221227161230-091c0ba34f0a // indirect
 	github.com/jackc/pgtype v1.14.0 // indirect
 	github.com/jackc/puddle v1.3.0 // indirect
+	github.com/takt-corp/fx-logrus v1.0.1 // indirect
+	go.uber.org/dig v1.17.1 // indirect
+	go.uber.org/fx v1.21.0 // indirect
 	go.uber.org/multierr v1.10.0 // indirect
 	golang.org/x/crypto v0.22.0 // indirect
 	golang.org/x/sys v0.19.0 // indirect
