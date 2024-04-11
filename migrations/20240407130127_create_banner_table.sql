95e2460be76e26b84cf5317470b10a50a34f5cb4
Roman Drozdov <romandrozdov@icloud.com>
2024-04-11 16:04:50 +0500

    Update migration structure.

--- migrations/20240407130127_create_banner_table.sql
+++ migrations/20240407130127_create_banner_table.sql
@@ -2,7 +2,7 @@
 CREATE TABLE banner (
     id SERIAL PRIMARY KEY,
     is_active BOOLEAN NOT NULL,
-    content TEXT,
+    content JSONB,
     feature_id INT,
     created_at TIMESTAMP WITHOUT TIME ZONE DEFAULT (NOW() AT TIME ZONE 'utc'),
     updated_at TIMESTAMP WITHOUT TIME ZONE,
