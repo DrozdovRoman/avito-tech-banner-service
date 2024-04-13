549655f0d8bda303f1268ccf0dc64ada7074ed06
Roman Drozdov <romandrozdov@icloud.com>
2024-04-13 15:51:26 +0500

    Update db structure again.

--- migrations/20240413151828_create_banner_table.sql
+++ migrations/20240413151828_create_banner_table.sql
@@ -1,10 +1,11 @@
 -- +goose Up
 CREATE TABLE banner (
     id SERIAL PRIMARY KEY,
+    is_active BOOLEAN NOT NULL,
     feature_id INT UNIQUE,
     content JSONB,
-    created_at TIMESTAMP WITHOUT TIME ZONE DEFAULT CURRENT_TIMESTAMP,
-    updated_at TIMESTAMP WITHOUT TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+    created_at TIMESTAMP WITHOUT TIME ZONE DEFAULT (NOW() AT TIME ZONE 'utc'),
+    updated_at TIMESTAMP WITHOUT TIME ZONE,
     FOREIGN KEY (feature_id) REFERENCES feature(id)
 );
 
